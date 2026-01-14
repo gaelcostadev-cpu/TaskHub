2361d3d4d0b846afff543a1c6dd91b9e84d06bee
gaelcostadev-cpu <gaelcostadev@gmail.com>
2026-01-13 22:00:22 -0300

    Add: Password hasher service and interface

---
+++ src/AuthService/Services/PasswordHasher.cs
@@ -0,0 +1,17 @@
+﻿using BCrypt.Net;
+
+namespace AuthService.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+        }
+    }
+}
