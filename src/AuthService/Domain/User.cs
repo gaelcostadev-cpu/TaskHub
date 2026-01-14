bdc9d88fdbe26ae80af08e712f3b8e2ecc12db33
gaelcostadev-cpu <gaelcostadev@gmail.com>
2026-01-13 21:58:47 -0300

    Add: User domain

---
+++ src/AuthService/Domain/User.cs
@@ -0,0 +1,39 @@
+﻿namespace AuthService.Domain
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+
+        public string Username { get; private set; }
+        public string Email { get; private set; }
+        public string PasswordHash { get; private set; }
+
+        public string? RefreshToken { get; private set; }
+        public DateTime? RefreshTokenExpiryTime { get; private set; }
+
+        public DateTime CreatedAt { get; private set; }
+
+        protected User() { }
+
+        public User(string username, string email, string passwordHash)
+        {
+            Id = Guid.NewGuid();
+            Username = username;
+            Email = email;
+            PasswordHash = passwordHash;
+            CreatedAt = DateTime.UtcNow;
+        }
+
+        public void UpdateRefreshToken(string refreshToken, DateTime expiryTime)
+        {
+            RefreshToken = refreshToken;
+            RefreshTokenExpiryTime = expiryTime;
+        }
+
+        public void RevokeRefreshToken()
+        {
+            RefreshToken = null;
+            RefreshTokenExpiryTime = null;
+        }
+    }
+}
