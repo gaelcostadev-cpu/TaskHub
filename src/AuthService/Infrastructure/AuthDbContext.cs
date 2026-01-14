a9431edc7c32f8d860e3703274b06f9af7ffaa77
gaelcostadev-cpu <gaelcostadev@gmail.com>
2026-01-13 21:59:30 -0300

    Add: auth database context

---
+++ src/AuthService/Infrastructure/AuthDbContext.cs
@@ -0,0 +1,47 @@
+﻿using AuthService.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Infrastructure {
+    public class AuthDbContext : DbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var user = modelBuilder.Entity<User>();
+
+            user.ToTable("Users");
+
+            user.HasKey(u => u.Id);
+
+            user.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(256);
+
+            user.Property(u => u.Username)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            user.Property(u => u.PasswordHash)
+                .IsRequired();
+
+            user.Property(u => u.RefreshToken)
+                .HasMaxLength(512);
+
+            user.Property(u => u.CreatedAt)
+                .IsRequired();
+
+            user.HasIndex(u => u.Email)
+                .IsUnique();
+
+            user.HasIndex(u => u.Username)
+                .IsUnique();
+        }
+    }
+}
