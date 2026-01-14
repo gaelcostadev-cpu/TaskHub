1f25acc0a5274b6b5daaf2fdee814dc0e5e646aa
gaelcostadev-cpu <gaelcostadev@gmail.com>
2026-01-13 22:00:37 -0300

    Add: register request and response

---
+++ src/AuthService/Contracts/RegisterRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Contracts
+{
+    public class RegisterRequest
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MinLength(3)]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ src/AuthService/Contracts/RegisterResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthService.Contracts
+{
+    public class RegisterResponse
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = null!;
+        public string Username { get; set; } = null!;
+    }
+}
